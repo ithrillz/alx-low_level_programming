ab202b05e27a35e42f0286a24de2972b7b7b938b
ithrillz <wisdomibokette@gmail.com>
2022-09-09 04:10:14 +0000

    Putting in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - main block
+ * main - main block
  * Return: 0
  **/
 int main(void)
 {
-puts(stderr, "\"Programming is like building a multinligual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
