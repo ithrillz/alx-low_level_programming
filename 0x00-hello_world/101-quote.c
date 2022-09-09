97cff024512a3a0c62b29516635f39352e3fc5c2
ithrillz <wisdomibokette@gmail.com>
2022-09-09 04:30:53 +0000

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
-int main (void)
+/**
+ * main - Entry point
+ * Return: 1
+ */
+int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
-	return (1);
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(2, ch, sizeof(ch));
+return (1);
 }
